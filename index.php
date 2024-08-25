d444a26f4d48e27885064c0ff0fc4a348ac26da9
christopher <ochristopherfilipe@gmail.com>
2024-08-25 15:43:27 +0000

    teste 2 separando html

--- index.php
+++ index.php
@@ -9,25 +9,21 @@ use PHPMailer\PHPMailer\Exception;
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-if(isset($_POST['submit'])){
-
-
+if(isset($_POST['submit'])) {
     // Obtendo dados do formulário
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     $descricao_servicos = $_POST['descricao_servicos'];
+    $tipo_servico = $_POST['tipo_servico'];
 
-
-
-    // Envie um e-mail para o endereço profissional
-    enviarEmail($nome, $email, $telefone, $descricao_servicos);
+    // Enviar e-mail para o endereço profissional
+    enviarEmail($nome, $email, $telefone, $descricao_servicos, $tipo_servico);
 
     echo ("<script>alert('Dados Enviados Com Sucesso!')</script>");
-    
 }
 
-function enviarEmail($nome, $email, $telefone, $descricao_servicos) {
+function enviarEmail($nome, $email, $telefone, $descricao_servicos, $tipo_servico) {
     // Configurações do PHPMailer
     $mail = new PHPMailer(true);
     $mail->SMTPDebug = SMTP::DEBUG_OFF;
@@ -41,20 +37,20 @@ function enviarEmail($nome, $email, $telefone, $descricao_servicos) {
     $mail->CharSet    = "utf8";
 
     // Destinatário do e-mail
-    $mail->setFrom('Maria@cadastro.msprojetosambientais.com.br', 'Cadastro MSPA'); 
+    $mail->setFrom('Maria@cadastro.msprojetosambientais.com.br', 'Cadastro MSPA');
     $mail->addAddress('maria.fsnery@gmail.com', 'Maria');
     $mail->addAddress('ms.pjambientais@gmail.com', 'MS Projetos Ambientais');
     $mail->addAddress('ochristopherfilipe@gmail.com', 'Christopher');
 
-
-// Conteúdo do e-mail
-$mail->isHTML(true);
-$mail->Subject = 'Nova solicitação de serviço';
-$mail->Body = "Nova solicitação de serviços no site MSprojetosambientais:<br><br>"
-    . "Nome: $nome<br>"
-    . "E-mail: $email<br>"
-    . "Telefone: $telefone<br>"
-    . "Descrição dos Serviços: $descricao_servicos<br>";
+    // Conteúdo do e-mail
+    $mail->isHTML(true);
+    $mail->Subject = 'Nova solicitação de serviço';
+    $mail->Body    = "Nova solicitação de serviços no site MSprojetosambientais:<br><br>"
+                   . "Nome: $nome<br>"
+                   . "E-mail: $email<br>"
+                   . "Telefone: $telefone<br>"
+                   . "Descrição dos Serviços: $descricao_servicos<br>"
+                   . "Tipo de Serviço: $tipo_servico<br>";
 
     // Enviar e-mail
     try {
@@ -65,61 +61,3 @@ $mail->Body = "Nova solicitação de serviços no site MSprojetosambientais:<br>
     }
 }
 ?>
-
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fale conosco</title>
-    <subtitle>Faça um orçamento sem compromisso</subtitle>
-    <link rel="icon" href="logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <div class="container">
-        <div class="box">
-            <form action="index.php" method="post">
-                <fieldset>
-                    <legend><b>Solicitação de Serviços</b></legend>
-                    <br>
-                    <div class="inputBox">
-                        <input type="text" name="nome" id="nome" class="inputUser" required>
-                        <label for="nome" class="labelInput">Nome completo</label>
-                    </div>
-                    <br><br>
-                    <div class="inputBox">
-                        <input type="text" name="email" id="email" class="inputUser" required>
-                        <label for="email" class="labelInput">Email</label>
-                    </div>
-                    <br><br>
-                    <div class="inputBox">
-                        <input type="tel" name="telefone" id="telefone" class="inputUser" required>
-                        <label for="telefone" class="labelInput">Telefone</label>
-                    </div>
-                    <br><br>
-                    <div class="inputBox">
-                        <textarea name="descricao_servicos" id="descricao_servicos" class="inputUser" rows="6"
-                            required></textarea>
-                        <label for="descricao_servicos" class="labelInput">Breve descrição dos serviços que você
-                            precisa:</label>
-                    </div>
-                    <br><br>
-                    <input type="submit" name="submit" id="submit">
-                </fieldset>
-            </form>
-        </div>
-    </div>
-</body>
-
-</html>
