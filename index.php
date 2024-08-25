0d0fde5e748ff1c81bf86745132c3a3141d15390
christopher <ochristopherfilipe@gmail.com>
2024-08-25 15:37:55 +0000

    teste1

--- index.php
+++ index.php
@@ -10,38 +10,24 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 if(isset($_POST['submit'])){
-    include_once('conexao.php');
 
-    // Verificar a conexão com o banco de dados
-    if (!$con) {
-        die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
-    }
 
     // Obtendo dados do formulário
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
-    $genero = $_POST['genero'];
-    $data_nascimento = $_POST['data_nascimento'];
-    $cidade = $_POST['cidade'];
-    $estado = $_POST['estado'];
     $descricao_servicos = $_POST['descricao_servicos'];
 
-    // Inserindo dados no banco de dados
-    $envio = mysqli_query($con, "INSERT INTO clientes (id, nome, email, telefone, genero, data_nascimento, cidade, estado, descricao_servicos) VALUES (null, '$nome', '$email', '$telefone', '$genero', '$data_nascimento', '$cidade', '$estado', '$descricao_servicos')");
 
-    // Verificando se houve falha na inserção
-    if(!$envio) {
-        echo("<script>alert('Falha ao Enviar Dados')</script>");
-    } else {
-        // Envie um e-mail para o endereço profissional
-        enviarEmail($nome, $email, $telefone, $genero, $data_nascimento, $cidade, $estado, $descricao_servicos);
 
-        echo ("<script>alert('Dados Enviados Com Sucesso!')</script>");
-    }
+    // Envie um e-mail para o endereço profissional
+    enviarEmail($nome, $email, $telefone, $descricao_servicos);
+
+    echo ("<script>alert('Dados Enviados Com Sucesso!')</script>");
+    
 }
 
-function enviarEmail($nome, $email, $telefone, $genero, $data_nascimento, $cidade, $estado, $descricao_servicos) {
+function enviarEmail($nome, $email, $telefone, $descricao_servicos) {
     // Configurações do PHPMailer
     $mail = new PHPMailer(true);
     $mail->SMTPDebug = SMTP::DEBUG_OFF;
@@ -64,15 +50,11 @@ function enviarEmail($nome, $email, $telefone, $genero, $data_nascimento, $cidad
 // Conteúdo do e-mail
 $mail->isHTML(true);
 $mail->Subject = 'Nova solicitação de serviço';
-$mail->Body = "Novo cadastro e nova solicitação de serviços:<br><br>"
+$mail->Body = "Nova solicitação de serviços no site MSprojetosambientais:<br><br>"
     . "Nome: $nome<br>"
     . "E-mail: $email<br>"
     . "Telefone: $telefone<br>"
-    . "Gênero: $genero<br>"
-    . "Data de Nascimento: $data_nascimento<br>"
-    . "Cidade: $cidade<br>"
-    . "Estado: $estado<br>"
-    . "Descrição dos Serviços: $descricao_servicos";
+    . "Descrição dos Serviços: $descricao_servicos<br>";
 
     // Enviar e-mail
     try {
@@ -98,7 +80,8 @@ $mail->Body = "Novo cadastro e nova solicitação de serviços:<br><br>"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro MSPA</title>
+    <title>Fale conosco</title>
+    <subtitle>Faça um orçamento sem compromisso</subtitle>
     <link rel="icon" href="logo.png" type="image/x-icon">
     <link rel="stylesheet" href="style.css">
 </head>
@@ -123,28 +106,6 @@ $mail->Body = "Novo cadastro e nova solicitação de serviços:<br><br>"
                     <div class="inputBox">
                         <input type="tel" name="telefone" id="telefone" class="inputUser" required>
                         <label for="telefone" class="labelInput">Telefone</label>
-                    </div>
-                    <p>Sexo:</p>
-                    <input type="radio" id="feminino" name="genero" value="feminino" required>
-                    <label for="feminino">Feminino</label>
-                    <br>
-                    <input type="radio" id="masculino" name="genero" value="masculino" required>
-                    <label for="masculino">Masculino</label>
-                    <br>
-                    <input type="radio" id="outro" name="genero" value="outro" required>
-                    <label for="outro">Outro</label>
-                    <br><br>
-                    <label for="data_nascimento"><b>Data de Nascimento:</b></label>
-                    <input type="date" name="data_nascimento" id="data_nascimento" required>
-                    <br><br><br>
-                    <div class="inputBox">
-                        <input type="text" name="cidade" id="cidade" class="inputUser" required>
-                        <label for="cidade" class="labelInput">Cidade</label>
-                    </div>
-                    <br><br>
-                    <div class="inputBox">
-                        <input type="text" name="estado" id="estado" class="inputUser" required>
-                        <label for="estado" class="labelInput">Estado</label>
                     </div>
                     <br><br>
                     <div class="inputBox">
